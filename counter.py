ddeb56413ca28f38d20c268fd20dfc25c601561e
ivo <ivailomet@gmail.com>
2018-06-16 10:22:16 +0300

    many improvements in the main script

--- counter.py
+++ counter.py
@@ -1,31 +1,64 @@
-#text = open("text.txt", "r")
-text = open("2600-0.txt", "r")
-badWords = open("badWords.txt", "r")
-wordCount = 0
+#!/usr/bin/python3
+
+import sys
+import urllib.request
+import os.path
+import hashlib
+from myLib import downloadBook
 
 uniqueWords = {}
 topWords = {}
-
 allWords = []
 excludedWords = []
 
-for line in badWords:
-    for word in line.split(','):
-        word = word.replace(" ","")
-        excludedWords.append(word)
+defaultTopPrint = 50
+bookCheckSum = "2c89aeaa17956a955d789fb393934b9a"
+bookUrl = "http://www.gutenberg.org/files/2600/2600-0.txt"
+wordCount = 0
 
-for line in text:
-    for word in line.split():
-        wordCount = wordCount + 1
-        if word not in excludedWords:
-            word = word.lower()
-            word = word.replace(".","")
-            word = word.replace(",","")
-            word = word.replace("!","")
-            word = word.replace(":","")
-            word = word.replace("?","")
-            word = word.replace("*","")
-            allWords.append(word)
+def removePunctuation():
+    global wordCount
+    for line in text:
+        for word in line.split():
+            wordCount = wordCount + 1
+            if word not in excludedWords:
+                word = word.lower()
+                word = word.replace(".","")
+                word = word.replace(",","")
+                word = word.replace("!","")
+                word = word.replace(":","")
+                word = word.replace("?","")
+                word = word.replace("*","")
+                allWords.append(word)
+
+def isBookHere():
+    bookHere = False
+    for file in os.listdir("."):
+        if file.endswith(".txt"):
+            if bookCheckSum == hashlib.md5(open(file, 'rb').read()).hexdigest():
+                bookHere = True
+                print ("the book is here!")
+                break
+    return bookHere
+
+def printResults():
+    print ("word --> occurances")
+    print ("*******************************")
+    numberOfWords = int(sys.argv[1] if len(sys.argv) >= 2 else defaultTopPrint)
+    topWords = sorted(uniqueWords.items(), key=lambda x:-x[1])[:numberOfWords]
+    for word, count in topWords:
+        print ("%s --> %s" %(word, count))
+    print ("*******************************")
+    print ("*******************************")
+    print ("total words : %d" %wordCount)
+
+if not isBookHere():
+    downloadBook()
+
+text = open("book.txt", "r")
+badWords = open("badWords.txt", "r")
+
+removePunctuation()
 
 for word in allWords:
     if word not in uniqueWords:
@@ -33,11 +66,4 @@ for word in allWords:
     else:
         uniqueWords[word] = uniqueWords[word] + 1
 
-print "word --> occurances"
-print "*******************************"
-topWords = sorted(uniqueWords.iteritems(), key=lambda x:-x[1])[:50]
-for word, count in topWords:
-    print "%s --> %s" %(word, count)
-print "*******************************"
-print "*******************************"
-print "total words : %d" %(wordCount)
+printResults()
